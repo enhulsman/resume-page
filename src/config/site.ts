6cbe22bdff4ca95af11675a841da609a783f2417
enhulsman <ezra@hulsman.dev>
2025-08-26 17:05:36 +0200

    Centralized gravatar logic + added small icon to Nav outside of about page

--- src/config/site.ts
+++ src/config/site.ts
@@ -2,9 +2,10 @@ const site = {
   name: 'Ezra Hulsman',
   role: 'Fullstack / Systems',
   email: 'info@hulsman.dev',
-  location: 'Earth',
+  location: 'Netherlands 🇳🇱',
   description: 'Software engineer focused on fast, robust systems and delightful UIs.',
-  skills: ['Astro', 'TypeScript', 'React', 'Rust'],
+  // skills: ['Astro', 'TypeScript', 'React', 'Rust'],
+  skills: ['Python', 'React', 'Rust', 'C/C++', 'Java', 'Bash'],
   social: {
     github: 'https://github.com/arc891',
     linkedin: 'https://www.linkedin.com/in/ezrahulsman',
